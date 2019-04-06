47dd5cc09274bb19db87a7f739b3dd4eebcc3ad8
anxious-pineapple <46401235+anxious-pineapple@users.noreply.github.com>
2019-04-06 17:19:23 +0530

    made coefficient more accurate
    made it a function of the plot, by minimising

--- random walk.py
+++ random walk.py
@@ -2,11 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
+
 no_readings=10000
 gap=1                       #gap b/w each plotted time
 start=0                     #starting value
 mu=0                        #mean for random variable
-sigma=20                     #sigma for random variable
+sigma=10                #sigma for random variable
 
 
 
@@ -24,6 +25,14 @@ def random_walk(no_readings,signal):
 def gaussian_walk(no_readings,signal):
     for i in range(1,no_readings):
         signal[i]=(np.random.normal(mu, sigma, 1))
+        
+
+def fit_error_rms(actual,fit):
+    error=np.empty([len(actual),], dtype=float)
+    for i in range(len(actual)):
+        error[i]=((actual[i]-fit[i])**2)
+    return error
+    
 
 
 random_walk(no_readings, signal1)
@@ -47,17 +56,37 @@ freqs2, psd2 = signal.welch(signal2)
 fit=np.empty([len(freqs1),], dtype=float)
 for i in range(len(freqs1)):
     if freqs1[i]!=0:
-        fit[i]=(freqs1[i])**(-2)
+        fit[i]=7*(freqs1[i])**(-2)
     else:
         fit[i]=0
+
+K_num=0
+K_denom=0
+for i in range(len(psd1)):
+    if freqs1[i]!=0:
+        K_denom+=(freqs1[i])**(-4)
+        K_num+=((freqs1[i])**(-2))*psd1[i]
+K=K_num/K_denom
+fit3=np.empty([len(freqs1),], dtype=float)
+for i in range(len(freqs1)):
+    if freqs1[i]!=0:
+        fit3[i]=K*(freqs1[i])**(-2)
+    else:
+        fit3[i]=0
+print(K)
+
+#e_guess= fit_error_rms(psd1,fit)
+#e_calc= fit_error_rms(psd1,fit3)
 
 plt.subplot(1,3,2)
 plt.semilogx(freqs1, psd1,'r-')
 plt.semilogx(freqs2, psd2,'b-')
-plt.semilogx(freqs1, fit,'go')
+#plt.semilogx(freqs1, fit,'go')                     #guessed coeff
+plt.semilogx(freqs1, fit3,'mo')                        #calculated coeff
 plt.title('PSD: power spectral density')
 plt.xlabel('Frequency')
 plt.ylabel('Power')
+
 
 '''
 plt.subplot(1,3,3)
